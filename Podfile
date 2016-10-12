3cb722407e9029be361dbc9bcfb731053dda2034
TySchultz <Tyschultz7@gmail.com>
2016-10-11 21:04:16 -0400

    testing IG

---
+++ Podfile
@@ -0,0 +1,6 @@
+use_frameworks!
+
+target 'Sports' do
+  pod 'SnapKit', '~> 3.0.0'
+  pod 'IGListKit'
+end
---
+++ Sports/Game.swift
@@ -0,0 +1,26 @@
+//
+//  Game.swift
+//  TestNFLGames
+//
+//  Created by Ty Schultz on 8/19/16.
+//  Copyright © 2016 Ty Schultz. All rights reserved.
+//
+
+import Foundation
+
+class Game: NSObject {
+    dynamic var homeTeam: String   = ""
+    dynamic var awayTeam: String   = ""
+    dynamic var date: String      = ""
+    dynamic var homeScore: String = ""
+    dynamic var awayScore: String = ""
+    dynamic var gameClock: String = ""
+    dynamic var gameStart: String = ""
+    dynamic var quarter: String = ""
+    dynamic var id: String = ""
+    dynamic var gameWeek: Int = 0
+
+}
+
+
+
